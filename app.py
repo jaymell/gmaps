e34f9bb8c1d192849b88f3faa7ddaa3db9f10583
james <james@jnt-mobile.jnt>
2015-06-25 23:55:51 -0500

    . Server now pulls data from DB (supplied by 'top_sites.py') 	and serves it out via site . client-side parses that and creates markers for each IP 	on the site

--- app.py
+++ app.py
@@ -7,9 +7,22 @@ import json
 
 app = Flask(__name__)
 
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DB_NAME = 'top_sites'
+COLLECTION_NAME = 'sites'
+connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+collection = connection[DB_NAME][COLLECTION_NAME]
+
 @app.route("/")
 def index():
 	return render_template("gmaps.html")
 
+@app.route("/json")
+def get_json():
+	sites = [ i for i in collection.find({}, {'_id': False}) ]
+	sites = json.dumps(sites, default=json_util.default)
+	return sites
+	
 if __name__ == "__main__":
 	app.run(host='localhost',port=5000,debug=True)
